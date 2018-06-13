c1d5bf32bb2e7b70bac88e5702f3e5cdffcfb61e
Trac Nguyen <nguyendtrac@gmail.com>
2018-06-13 22:32:03 +0000

    automatically backed up by learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -14,6 +14,8 @@ class Genre < ActiveRecord::Base
 
   def all_artist_names
     # return an array of strings containing every musician's name
-    self.artists
+    self.artists.map do |x|
+      x.name
+    end
   end
 end
